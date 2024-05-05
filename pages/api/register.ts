65028210dfc75649dd8d4dad6131516de4af73e9
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-05-05 18:50:11 +0530

    register file modification

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -3,9 +3,9 @@ import {NextApiRequest,NextApiResponse} from 'next';
 import prismadb from '@/lib/prismadb';
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse) {
-    if(req.method !== 'POST'){
-        return res.status(405).end()
-    }
+    // if(req.method !== 'POST'){
+    //     return res.status(405).end()
+    // }
     try {
        const {email,password,name} =req.body;
        const existingUser=await prismadb.user.findUnique({
